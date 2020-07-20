3355c0352d18c12b6aa7c49d25409e6b059e4892
Abhay Bhardwaj <markunkfoevajacobs@gmail.com>
2020-07-20 20:42:06 +0530

    carousal fixed

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import noUiSlider from "nouislider";
-
+declare let $: any;
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
@@ -21,6 +21,41 @@ export class LandingPageComponent implements OnInit, OnDestroy {
         this.router.navigate(["/matchmakers"]);
   }
   ngOnInit() {
+    $('.responsive').slick({
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2300,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+      
     var body = document.getElementsByTagName("body")[0];
     body.classList.add("index-page");
 
@@ -52,7 +87,6 @@ export class LandingPageComponent implements OnInit, OnDestroy {
     {img: "http://placehold.it/350x150/333333"},
     {img: "http://placehold.it/350x150/666666"}
   ];
-  slideConfig = {"slidesToShow": 3, "slidesToScroll": 3};
   
   addSlide() {
     this.slides.push({img: "http://placehold.it/350x150/777777"})
